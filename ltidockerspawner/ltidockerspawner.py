8914b64796478c858a652a485760efd550d89617
Xavi Aracil <xavi@laibeth.com>
2016-06-21 10:35:41 +0200

    Initial skeleton

---
+++ ltidockerspawner/ltidockerspawner.py
@@ -0,0 +1,10 @@
+import DockerSpawner as DockerSpawner
+
+
+class LtiDockerSpawner(DockerSpawner):
+    """
+    DockerSpawner that defines notebook_dir and container_image
+    from LTI (http://www.imsglobal.org/activity/learning-tools-interoperability) context
+    """
+    pass
+
