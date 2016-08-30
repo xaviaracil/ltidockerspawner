b6e7869ec369364b484fbd8e9a251cf8efdfdb8b
Xavi Aracil <xavi@laibeth.com>
2016-08-30 11:15:09 +0200

    Context notebooks are initialized in spawner using "put_archive" docker library method

--- ltidockerspawner/ltidockerspawner.py
+++ ltidockerspawner/ltidockerspawner.py
@@ -12,11 +12,12 @@ class LTIDockerSpawner(DockerSpawner, LTIAwareMixin):
     from LTI (http://www.imsglobal.org/activity/learning-tools-interoperability) context
     """
 
-    notebook_root_dir = Unicode(
+    notebooks_tar_file = Unicode(
         '',
         config=True,
         allow_none=True,
-        help="""Path where find notebooks for every context. If not found, notebook_dir is used as default"""
+        help=("Path to a tar file where find notebooks for every context. \n"
+              "             If not found, notebook_dir is used as default")
     )
 
     container_image_param_name = Unicode(
@@ -30,32 +31,29 @@ class LTIDockerSpawner(DockerSpawner, LTIAwareMixin):
     def _fmt(self, v):
         return v.format(context_id=self.provider.context_id)
 
-    def get_env(self):
-        env = super().get_env()
-        if self.notebook_root_dir:
-            volume_dir = self._fmt(self.notebook_root_dir)
-            self.log.info(
-                "notebook_root_dir present with value %s. Formatted: %s",
-                self.notebook_root_dir, volume_dir)
-            env["NOTEBOOK_DIR"] = volume_dir
-
-        return env
-
     @gen.coroutine
     def start(self, image=None, extra_create_kwargs=None, extra_start_kwargs=None, extra_host_config=None):
+
+        container = yield self.get_container()
+        do_copy_notebooks = container is None and self.notebooks_tar_file
 
         """Get image name from LTI context"""
         if self.container_image_param_name:
             image = self.provider.get_custom_param(self.container_image_param_name)
 
-        """Get notebook dir from LTI context"""
-        if self.notebook_root_dir:
-            volume_dir = self._fmt(self.notebook_root_dir)
-            context_volume = {self.provider.context_label: volume_dir}
-            if not extra_host_config:
-                extra_host_config = dict(binds=self._volumes_to_binds(context_volume, {}))
-            else:
-                extra_host_config['binds'] = self._volumes_to_binds(context_volume, extra_host_config['binds'])
-
         """Start the server"""
         yield super().start(image, extra_create_kwargs, extra_start_kwargs, extra_host_config)
+
+        if do_copy_notebooks:
+            "Copy notebooks_tar_file to data volume"
+            context_tar_file = self._fmt(self.notebooks_tar_file)
+            tar_file = open(context_tar_file, "rb")
+            tar_data = tar_file.read()
+            # build the dictionary of keyword arguments for put_archive
+            create_kwargs = dict(
+                container=self.container_id,
+                path=self.notebook_dir,
+                data=tar_data)
+
+            yield self.docker('put_archive', **create_kwargs)
+            tar_file.close()
